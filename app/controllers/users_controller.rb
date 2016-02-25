302838facdee3aac3e2299a0cfa6bbd7483e156e
Tony Blum <tonyblum@me.com>
2016-02-25 12:37:47 -0800

    config mailer to send email on user creation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -28,6 +28,7 @@ class UsersController < ApplicationController
 
     respond_to do |format|
       if @user.save
+        UserMailer.registration_confirmation(@user).deliver
         format.html { redirect_to @user, notice: 'User was successfully created.' }
         format.json { render :show, status: :created, location: @user }
       else
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,2 +1,9 @@
 class UserMailer < ApplicationMailer
+  default from:  "tonyblum@me.com"
+
+  def registration_confirmation(user)
+    @user = user
+    mail   to: user.email, subject: "Registered"
+  end
+  
 end
